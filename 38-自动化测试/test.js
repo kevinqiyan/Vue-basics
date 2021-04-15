1950cb7fa262b6183cc14f2d5273a1d5e290cc68
guokw1 <kevin1025253@gmail.com>
2021-04-15 17:20:33 +0800

    监听DOM结构变化(webdriver bidi apis(正确的使用方法待确认)

--- 38-自动化测试/test.js
+++ 38-自动化测试/test.js
@@ -3,11 +3,21 @@ var xlsx = require('node-xlsx');
 var fs = require('fs');
 const { type } = require('os');
 var window = window;
+
 var dataExe = [];
 var dataArray = [];
 let nullArray = [];
+
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
+
+  // 监听特定元素的DOM结构变化,使用方法未知(待完善)
+  // const cdpConnection = await driver.createCDPConnection('page')
+  //   await driver.logMutationEvents(cdpConnection, function (event) {
+  //       assert.strictEqual(event['attribute_name'], 'style')
+  //       assert.strictEqual(event['current_value'], '')
+  //       assert.strictEqual(event['old_value'], 'display:none;')
+  //   })
   try {
     await driver.manage().setTimeouts({ implicit: 1000 });
 
@@ -21,45 +31,43 @@ let nullArray = [];
 
     await driver.get('测试数据网站');
     await driver.findElement(By.css('.custom_input')).sendKeys("4CEBBD3F8A07");
-    driver.findElement(By.id('guide_start_btn')).click();
+    driver.findElement(By.id('guide_start_btn')).click(); //页面button初始化状态->开始
+    driver.findElement(By.css('.tip_option .dropdown-toggle')).click()
+    driver.findElement(By.id('filter_str')).click();
+    driver.findElement(By.id('filter_str')).sendKeys("MC");
+    // let btnt = await driver.findElement(By.css('#act_start_btn')).getText()
+    // console.log(btnt,'文本');
 
-    // console.log(driver.findElement(By.id('guide_start_btn')),'11111');
-    // let array2 = driver.findElement(By.css('#query_result_table>tbody'))
-    // let seach = array2.findElement(By.name('tr'))
-    // console.log(seach,'search');
-    // var array =[]
-    // await driver.wait(until.elementLocated(By.css('#query_result_table tbody tr')),10000)
-    // await driver.manage().setTimeouts({ implicit: 10000 });
-    // let abb = driver.findElements(By.css('#query_result_table tbody tr'))
-    // let this_ = this.driver
-    // for (const e of abb) {
-    //   assert.strictEqual(await e.getText())
-    //   console.log( await e.getText());
+    // window.document.getElementById('#act_start_btn').onclick = function(){
+    //   let btnt =  driver.findElement(By.css('#act_start_btn')).getText()
+    //   console.log(btnt, '文本');
     // }
+    // let set = setInterval(() => {
+      // driver.navigate().refresh();
+      // let btnTxt = driver.findElement(By.css('#act_start_btn')).getText()
+      // let aa = driver.findElements(By.css('#query_result_table tbody tr'));
 
-    // array.then((res)=>{
-    //   console.log(res,'成功');
-    // }).catch((error)=>{
-    //   console.log(error,'失败');
-    // })
-    // console.log(array);
-    // for (let index = 0; index < array.length; index++) {
-    //   const element = array[index];
-    //   console.log(element);
-    // }
-    // let firstResult = await driver.wait(until.elementLocated(By.css('query_result_table>tbody>tr')), 10000);
-    // console.log(firstResult);
-    // driver.findElements(By.css('query_result_table>tbody>tr'))
-    // await driver.findElement(By.id('guide_start_btn')).click();
-    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+      // // clearInterval(set)
+      // let aac = aa.then((req)=>{
+      //   return req
+      // })
+      // console.log(btnTxt);
+      // while (aac&&btnTxt == '开始') {
+        
+      // clearInterval(set)
+      //   getData()
+      // }
+    // }, 10000);
+    
+
   } finally {
     console.log('文档执行成功了');
   }
-  setTimeout(function () {
-
-    driver.findElement(By.id('act_start_btn')).click();
+  function getData() {
+    
+    // driver.findElement(By.id('act_start_btn')).click(); // 更改页面button装填-> 停止
     let abb = driver.findElements(By.css('#query_result_table tbody tr'))
-    // console.log(typeof abb,abb);
+
     abb.then((req) => {
 
       for (const e of req) {
@@ -70,37 +78,53 @@ let nullArray = [];
 
       }
       setTimeout(() => {
-        // dataExe = xlsx.build([{name:'表格',data:dataArray}]);
-        // JSON.stringify(dataArray)
-        // let abcd = JSON.stringify(dataArray)
+
         let abcd = dataArray
         for (let index = 0; index < abcd.length; index++) {
           const element = abcd[index];
-          //  nullArray.push(element)
+
           nullArray.push(element.split(','))
         }
-        // console.log(nullArray);
+
         fs.writeFileSync('./数据.txt', JSON.stringify(nullArray))
         fs.writeFileSync('./数据--.txt', JSON.stringify(dataArray))
-        dataExe = xlsx.build([{ name: '表格', data: nullArray}]);
+        dataExe = xlsx.build([{ name: '表格', data: nullArray }]);
         fs.writeFileSync('./表格.xlsx', dataExe)
-        // fs.writeFileSync('./数据.txt',abcd)
-        // console.log(dataArray.split(','));
-        // let abc = dataArray.split(',');
-        // for (let index = 0; index < abc.length; index++) {
-        //   const element = abc[index];
-        //   dataExe.push(element)
-        // }
-        // console.log(dataExe);
-        // dataExe = xlsx.build([{name:'表格',data:dataArray}]);
-        // fs.writeFileSync('./表格.xlsx',dataExe)
+
+        driver.quit()
       }, 5000)
     })
-    // console.log(dataExe);
+  }
+  //  setTimeout(function () {
 
-    // 输出为exel文件
-    // dataExe = xlsx.build([{name:'表格',data:dataArray}]);
-    // fs.writeFileSync('./表格.xlsx',dataExe,'binary')
+  //   driver.findElement(By.id('act_start_btn')).click();
+  //   let abb = driver.findElements(By.css('#query_result_table tbody tr'))
 
-  }, 5000);
+  //   abb.then((req) => {
+
+  //     for (const e of req) {
+  //       e.getText().then((res) => {
+  //         dataArray.push(res);
+
+  //       })
+
+  //     }
+  //     setTimeout(() => {
+
+  //       let abcd = dataArray
+  //       for (let index = 0; index < abcd.length; index++) {
+  //         const element = abcd[index];
+
+  //         nullArray.push(element.split(','))
+  //       }
+
+  //       fs.writeFileSync('./数据.txt', JSON.stringify(nullArray))
+  //       fs.writeFileSync('./数据--.txt', JSON.stringify(dataArray))
+  //       dataExe = xlsx.build([{ name: '表格', data: nullArray }]);
+  //       fs.writeFileSync('./表格.xlsx', dataExe)
+
+  //     }, 5000)
+  //   })
+
+  // }, 10000);
 })();
