d98cabd84148b33966db39bf385b81341b23682c
guokw <kevin10252@163.com>
2022-08-09 15:47:54 +0800

    node练习编写接口测试

--- 41-node,js/node-express服务.js
+++ 41-node,js/node-express服务.js
@@ -1,14 +1,20 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
-app.get('/', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");  // 允许所有路径跨域
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.statusCode = 200;
-    res.send('hellow world');
-    // res.write('{"id":"123",name:"jack",arg:11111}')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/test', (req, res) => {
+    const body = req.query
+    res.send({
+        status: 200,
+        data: body,
+        message: '请求成功'
+    })
 })
+
 app.listen(3000, () => {
     console.log('服务器运行成功');
 })
+
