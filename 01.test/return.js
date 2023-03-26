a244a9a66fc8527ef42cc088609bc1c867111b9c
kevinqiyan <kevin1025253@gmail.com>
2023-03-26 15:12:18 +0800

    脚本更新

---
+++ 01.test/return.js
@@ -0,0 +1,13 @@
+function get(a) {
+    if (a == 1) {
+        return
+    }
+    return true
+}
+
+let abc = get(1)
+if (abc) {
+    console.log('111', abc)
+} else {
+    console.log('222');
+}
--- 45-油猴脚本学习/青书学堂.js
+++ 45-油猴脚本学习/青书学堂.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         青书学堂_仅自动播放
+// @name         青书学堂_自动播放
 // @namespace    guokwa.com
-// @version      0.1
-// @description  青书学堂自动后台静音播放
+// @version      0.3
+// @description  青书学堂自动后台静音播放，章节跳过。
 // @author       七言
 // @match        *://*.qingshuxuetang.com/*
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @require           https://unpkg.com/jquery@3.6.0/dist/jquery.min.js
-// @license MI
+// @license GPL
 // ==/UserScript==
 /* jshint esversion: 6 */
 
@@ -19,53 +19,9 @@
         var href = window.location.href
         if (href.indexOf('nodeId') > -1) {
             setTimeout(() => {
-                let video = document.getElementsByTagName('video')[0]
                 let params = new UrlSearch()
-                /* 
-                courseId: "1894"
-                nodeId: "kcjs_5_3"
-                periodId: "18"
-                teachPlanId: "460"
-                */
-                let courseId = params.courseId
-                let courseArr = params.nodeId.split('_')
-                let nextKey = "" // 下一个视频的key
-                if (!video) {
-                    console.log('找不到视频');
-                    if (courseArr.length == 2) {
-                        nextKey = `kcjs_${Number(courseArr[1]) + 1}`
-                    } else if (courseArr.length == 3) {
-                        nextKey = `kcjs_${Number(courseArr[1]) + 1}_${Number(1)}`
-                    }
-                    const nextUrl = `https://${window.location.host}${window.location.pathname}?teachPlanId=${params.teachPlanId}&periodId=${params.periodId}&courseId=${courseId}&nodeId=${nextKey}`
-                        console.log('下一个视频的链接_查找视频',nextUrl);
-                    location.replace(nextUrl)
-                } 
-                createHTML()
-                addStyle()
-                // 获取播放速度参数
-                let speedData = GM_getValue('Speed')
-                if (speedData) {
-                    video.playbackRate = speedData
-                } else {
-                    video.playbackRate = 1 // 默认一倍速播放
-                }
-                video.muted = true // 静音播放
-                //video.playbackRate = 1 // 倍速播放
-                video.play()
-                if (courseArr.length == 2) {
-                    nextKey = `kcjs_${Number(courseArr[1]) + 1}`
-                } else if (courseArr.length == 3) {
-                    nextKey = `kcjs_${courseArr[1]}_${Number(courseArr[2]) + 1}`
-                }
-                const nextUrl = `https://${window.location.host}${window.location.pathname}?teachPlanId=${params.teachPlanId}&periodId=${params.periodId}&courseId=${courseId}&nodeId=${nextKey}`
-                console.log('下一个视频的链接_播放完成',nextUrl);
-                video.addEventListener('ended', function () {
-                    location.replace(nextUrl)
-                })
+                videoPlay(params)
             }, 5000);
-            // 打印播放进度
-            getVideoProgress()
         }
     }
 )()
@@ -88,13 +44,55 @@ function UrlSearch() {
 	}
 }
 
-// 检测当前播放的进度
-function getVideoProgress() {
-    setInterval(() => {
-        let video = document.getElementsByTagName('video')[0]
-        var currentTime = video.currentTime.toFixed(1)
-        console.log('当前的视频进度是多少', currentTime);
-    }, 10000);
+
+// 函数抽离
+function videoPlay(params) {
+    let video = document.getElementsByTagName('video')[0]
+    let currentTime = video.currentTime.toFixed(1)
+
+    if (!video || currentTime == 0.0) {
+        nexVideoUrl(video,params)
+    }
+    createHTML()
+    addStyle()
+    // 获取播放速度参数
+    let speedData = GM_getValue('Speed')
+    if (speedData) {
+        video.playbackRate = speedData
+    } else {
+        video.playbackRate = 1 // 默认一倍速播放
+    }
+    video.muted = true // 静音播放
+    video.play()
+    nexVideoUrl(video,params)
+
+}
+
+// 下一个视频播放链接
+function nexVideoUrl(video,params) {
+    let { courseId, teachPlanId, periodId } = params
+    let courseArr = params.nodeId.split('_')
+
+    let nextKey = "" // 下一个视频的key
+
+    if (courseArr.length == 2) {
+        nextKey = `kcjs_${Number(courseArr[1]) + 1}`
+    } else if (courseArr.length == 3) {
+        if (!video) {
+            nextKey = `kcjs_${Number(courseArr[1]) + 1}_${Number(1)}`
+        } else {
+            nextKey = `kcjs_${courseArr[1]}_${Number(courseArr[2]) + 1}`
+        }
+    }
+    const nextUrl = `https://${window.location.host}${window.location.pathname}?teachPlanId=${teachPlanId}&periodId=${periodId}&courseId=${courseId}&nodeId=${nextKey}`
+
+    if (!video) {
+        location.replace(nextUrl)
+    } else {
+        video.addEventListener('ended', function () {
+            location.replace(nextUrl)
+        })
+    }
 }
 
 // 设置倍速html 和 样式
@@ -113,18 +111,15 @@ function createHTML() {
     $('.one').click(()=>{
         video.playbackRate = 1
         GM_setValue('Speed', 1)
-        console.log('进行点击了—— 111倍速');
     })
 
     $('.two').click(()=>{
         video.playbackRate = 2
         GM_setValue('Speed', 2)
-        console.log('进行点击了—— 222倍速');
     })
     $('.thre').click(()=>{
         video.playbackRate = 3
         GM_setValue('Speed', 3)
-        console.log('进行点击了——  333倍速');
     })
 }
 function addStyle(){
@@ -137,6 +132,7 @@ function addStyle(){
         border: 1px solid #ccc;
         height: 100px;
         width: 100px;
+        background: white;
     }
     .btnList p {
         color: red;
